dbe2e82854e0c08bbf236527e4d822cfdfd2faa3
StellAuror <michalss07@gmail.com>
2024-12-02 00:35:52 +0100

    mockup version created by gpt

--- MeanF/huber.R
+++
@@ -1,9 +0,0 @@
-f.calc_huber <- (
-  huber_loss <- function(residuals, threshold) {
-    loss <- ifelse(abs(residuals) <= threshold,
-                   0.5 * residuals^2,
-                   threshold * (abs(residuals) - 0.5 * threshold))
-    return(loss)
-  }
-)
-
--- bin/format_metric.R
+++
@@ -1,3 +0,0 @@
-f.format_metric <- function(value, label = "", r = 2) {
-  value |> round(r) |> paste0("", ... = _, label)
-}
